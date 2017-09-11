0e2da7bf39dfc662dad85b2b0e5273c281038d3b
hexiao-o <460559761@qq.com>
2017-09-11 23:54:13 +0800

    fix

--- example/src/routes/Home/index.js
+++ example/src/routes/Home/index.js
@@ -4,6 +4,7 @@ import styles from './IndexPage.less';
 import { Button } from 'antd';
 import './antd.min.css';
 import { Broadcaster, Viewer } from '../../../../src';
+import * as Rematrix from 'rematrix';
 
 class _TEST__ extends React.Component {
   constructor(props) {
@@ -11,6 +12,10 @@ class _TEST__ extends React.Component {
     this.state = {
       width: 0,
       height: 0,
+      scale: {
+        x: 1,
+        y: 1,
+      }
     };
   }
 
@@ -18,32 +23,38 @@ class _TEST__ extends React.Component {
     const { dispatch, location, socket } = this.props;
     if (this.wrap) {
       setTimeout(() => {
+        const width = this.wrap.offsetWidth/2;
+        const height = this.wrap.offsetHeight;
         this.setState({
-          width: this.wrap.offsetWidth/2,
-          height: this.wrap.offsetHeight,
+          width,
+          height,
         });
         this.page = true;
         this.Broadcaster.PPTLayer.handleCreatePage({
           attr: {
-            href: './images/w44.jpg',
-            width: this.wrap.offsetWidth/2, height: this.wrap.offsetHeight,
+            href: 'https://ppt-cdn.class100.com/ppts/766/G5L8_1.png',
+            width: 960, height: 540,
           },
         });
         this.Viewer.PPTLayer.handleCreatePage({
           attr: {
-            href: './images/w44.jpg',
-            width: this.wrap.offsetWidth/2, height: this.wrap.offsetHeight,
+            href: 'https://ppt-cdn.class100.com/ppts/766/G5L8_1.png',
+            width: 960, height: 540,
           },
         });
+        this.handleScale({ width, height });
       }, 500);
 
     }
     window.addEventListener('resize', () => {
       if (this.wrap) {
+        const width = this.wrap.offsetWidth/2;
+        const height = this.wrap.offsetHeight;
         this.setState({
-          width: this.wrap.offsetWidth/2,
-          height: this.wrap.offsetHeight,
+          width,
+          height,
         });
+        this.handleScale({ width, height });
       }
     });
   }
@@ -57,14 +68,14 @@ class _TEST__ extends React.Component {
           type: 'video',
           attr: {
             href: './images/videoBG.png',
-            width, height,
+            width: 960, height: 540,
           },
         });
         this.Viewer.PPTLayer.handleCreatePage({
           type: 'video',
           attr: {
             href: './images/videoBG.png',
-            width, height,
+            width: 960, height: 540,
           },
         });
 
@@ -72,14 +83,14 @@ class _TEST__ extends React.Component {
       } else {
         this.Broadcaster.PPTLayer.handleCreatePage({
           attr: {
-            href: './images/w44.jpg',
-            width, height,
+            href: 'https://ppt-cdn.class100.com/ppts/766/G5L8_1.png',
+            width: 960, height: 540,
           },
         });
         this.Viewer.PPTLayer.handleCreatePage({
           attr: {
-            href: './images/w44.jpg',
-            width, height,
+            href: 'https://ppt-cdn.class100.com/ppts/766/G5L8_1.png',
+            width: 960, height: 540,
           },
         });
         this.page = true;
@@ -94,14 +105,14 @@ class _TEST__ extends React.Component {
       if (!this.page) {
         this.Broadcaster.PPTLayer.handleCreatePage({
           attr: {
-            href: './images/w44.jpg',
-            width, height,
+            href: 'https://ppt-cdn.class100.com/ppts/766/G5L8_1.png',
+            width: 960, height: 540,
           },
         });
         this.Viewer.PPTLayer.handleCreatePage({
           attr: {
-            href: './images/w44.jpg',
-            width, height,
+            href: 'https://ppt-cdn.class100.com/ppts/766/G5L8_1.png',
+            width: 960, height: 540,
           },
         });
 
@@ -111,67 +122,114 @@ class _TEST__ extends React.Component {
           type: 'video',
           attr: {
             href: './images/videoBG.png',
-            width, height,
+            width: 960, height: 540,
           },
         });
         this.Viewer.PPTLayer.handleCreatePage({
           type: 'video',
           attr: {
             href: './images/videoBG.png',
-            width, height,
+            width: 960, height: 540,
           },
         });
         this.page = false;
       }
     }
   };
+  handleScale = ({ width = 1, height = 1 }) => {
+    const dd = 16/9;
+    const df = width/height;
+    const scale = {};
+    if (dd >= df) {
+      scale.y = scale.x = (width)/960
+    } else {
+      scale.y = scale.x = (height)/540
+    }
+    this.setState({
+      scale,
+    })
+  };
   renderB = (className) => {
     const { dispatch, socket } = this.props;
     const { drawItems, selectItem, mouseInfo, wBToolsInfo, pptConfig } = socket;
+    const { scale, width, height } = this.state;
+    const t = Rematrix.translate(960/2, 540/2);
+    const s = Rematrix.scale(scale.x, scale.y);
+    // console.log(t);
+    const product = [t,s].reduce(Rematrix.multiply)
+    const style = {
+      width: 960,
+      height: 540,
+      // transform: `matrix3d(${product.join(',')})`,
+      transform: `scale(${scale.x}, ${scale.y})`,
+      transformOrigin: 'left top',
+    };
     return (
-      <Broadcaster
-        className={className}
-        items={drawItems || []}
-        width={this.state.width}
-        height={this.state.height}
-        selectItem={selectItem}
-        mouseInfo={mouseInfo}
-        wBToolsInfo={wBToolsInfo}
-        ref={e => this.Broadcaster = e}
-        onWbToolsChange={(wBToolsInfo) => {
-          dispatch({ type: 'socket/wbToolsChange', wBToolsInfo });
-        }}
-        onMouseChange={(mouse) => {
-          dispatch({ type: 'socket/mouseMove', mouseInfo: mouse });
-        }}
-        onDrawChange={(item) => {
-          dispatch({ type: 'socket/drawChange', item });
-        }}
-        onDeleteChange={(item) => {
-          dispatch({ type: 'socket/deleteChange', item });
-        }}
-        pptConfig={{
-          onPlayChange(paused) {
-            dispatch({ type: 'socket/playChagne', paused });
-          }
-        }}
-      />
+      <div
+        className={styles.box}
+        style={style}
+      >
+        <Broadcaster
+          className={className}
+          items={drawItems || []}
+          width={960}
+          height={540}
+          selectItem={selectItem}
+          mouseInfo={mouseInfo}
+          wBToolsInfo={wBToolsInfo}
+          ref={e => this.Broadcaster = e}
+          onWbToolsChange={(wBToolsInfo) => {
+            dispatch({ type: 'socket/wbToolsChange', wBToolsInfo });
+          }}
+          onMouseChange={(mouse) => {
+            dispatch({ type: 'socket/mouseMove', mouseInfo: mouse });
+          }}
+          onDrawChange={(item) => {
+            dispatch({ type: 'socket/drawChange', item });
+          }}
+          onDeleteChange={(item) => {
+            dispatch({ type: 'socket/deleteChange', item });
+          }}
+          pptConfig={{
+            onPlayChange(paused) {
+              dispatch({ type: 'socket/playChagne', paused });
+            }
+          }}
+        />
+      </div>
     );
   };
   renderV = () => {
     const { socket } = this.props;
     const { drawItems, selectItem, mouseInfo, wBToolsInfo, pptConfig } = socket;
+    const { scale, width, height } = this.state;
+    const t = Rematrix.translate(width/2, height/2);
+    const s = Rematrix.scale(scale.x, scale.y);
+    // console.log(t);
+    const product = [s].reduce(Rematrix.multiply)
+    const style = {
+      width: 960,
+      height: 540,
+      // transform: `matrix3d(${product.join(',')})`,
+      transform: `scale(${scale.x}, ${scale.y})`,
+      transformOrigin: 'left top',
+    };
     return (
-      <Viewer
-        ref={e => this.Viewer = e}
-        items={drawItems || []}
-        width={this.state.width}
-        height={this.state.height}
-        selectItem={selectItem}
-        mouseInfo={mouseInfo}
-        wBToolsInfo={wBToolsInfo}
-        pptConfig={pptConfig}
-      />
+      <div
+        className={styles.box}
+        style={style}
+      >
+        <Viewer
+          ref={e => this.Viewer = e}
+          items={drawItems || []}
+          width={960}
+          height={540}
+          selectItem={selectItem}
+          mouseInfo={mouseInfo}
+          wBToolsInfo={wBToolsInfo}
+          pptConfig={pptConfig}
+        />
+      </div>
     );
   };
   render() {
@@ -180,15 +238,15 @@ class _TEST__ extends React.Component {
       <div ref={wrap => this.wrap = wrap} className={styles.__TEST__}>
         <div style={{ position: 'relative', width: this.state.width, height: '100%', background: '#eee', float: 'left' }} className="">
           <h1 className={styles.title}>Broadcaster</h1>
-          <div className="">
+          <div style={{ overflow: 'hidden' }} className={styles.pptWrapMMM}>
             {this.renderB('Broadcaster-1')}
           </div>
           <div className={styles.pptBtnPrev}><Button onClick={this.handlePrev} type="primary">上一页</Button></div>
           <div className={styles.pptBtnNext}><Button onClick={this.handleNext} type="primary">下一页</Button></div>
         </div>
-        <div style={{ width: this.state.width, height: '100%', float: 'left' }} className="">
+        <div style={{ position: 'relative', width: this.state.width, height: '100%', float: 'left' }} className="">
           <h1 className={styles.title}>Viewer</h1>
-          <div className="">
+          <div style={{ overflow: 'hidden' }} className={styles.pptWrapMMM}>
             {this.renderV()}
           </div>
         </div>
